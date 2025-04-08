9b46229b618ac0a1b7fb0b88a711218bce23dab2
Thiago <thberenger27@gmail.com>
2025-04-08 20:10:33 -0300

    Criando autenticação

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 CORS(app) 
 
+# Autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # Modelagem
 # User (id, username, password)
 class User(db.Model, UserMixin):
