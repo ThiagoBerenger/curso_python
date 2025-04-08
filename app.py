152414ab750b1762c8ab845a782b3d7e4d792139
Thiago <thberenger27@gmail.com>
2025-04-08 19:09:07 -0300

    criando tabela de usuário

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 # Importação
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'  
 
 db = SQLAlchemy(app)
+CORS(app) 
 
 # Modelagem
+# User (id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
 # Produto (id, name, price, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -72,7 +81,23 @@ def update_product(product_id):
     db.session.commit()
     return jsonify({'message': 'Product updated successfully!'})
         
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'description': product.description
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)
 
+    #-45
+
+# Rota Raiz
 @app.route('/')
 def hello_world():
     return "Hello, World!"
