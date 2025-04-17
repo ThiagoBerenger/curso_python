a4e1d405b3dce49300a814333740327d99126b20
Thiago <thberenger27@gmail.com>
2025-04-17 17:07:20 -0300

    Função que deleta todos os itens do carrinho

--- app.py
+++ app.py
@@ -176,6 +176,15 @@ def view_cart():
             })
     return jsonify(cart_content)
 
+@app.route('/api/cart/checkout', methods=['POST'])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({'message': 'Checkout successful. Cart has been cleaned.'})
 
 if __name__ == '__main__':
     app.run(debug=True)
